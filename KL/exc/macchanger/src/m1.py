0bb9492e0a5334b1176fa972af42bcb00692a5e0
kachraz <110726496+kachraz@users.noreply.github.com>
2024-12-31 02:19:19 +0000

    🫦

--- KL/exc/macchanger/src/m1.py
+++ KL/exc/macchanger/src/m1.py
@@ -1,1 +1,9 @@
 # Version of macchanger program
+def subp1():
+    label("mac_changer")
+    """Sunprocess function test"""
+    subprocess.run(["ifconfig"])
+    rprint("[cyan] Executing the macchanger three times[/cyan]")
+    for i in range(3):
+        subprocess.run(["macchanger", "-r", "eth0"])
+        rprint("[cyan] --- [/cyan]")
