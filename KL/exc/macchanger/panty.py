82975eaabb1c0c5ae57c5130c6eb6232a2b06ac4
kachraz <110726496+kachraz@users.noreply.github.com>
2024-12-31 02:44:29 +0000

    🫦

--- KL/exc/macchanger/panty.py
+++ KL/exc/macchanger/panty.py
@@ -1,10 +1,10 @@
 # main python which executes functtions 
 
 from src.m1 import mac1
-from src.m2 import mac2, mac21
+from src.m2 import mac2, mac21, mac23
 
 def main():
-    mac21()
+    mac23()
 
 
 if __name__ == "__main__":
