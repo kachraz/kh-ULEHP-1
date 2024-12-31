45e60d8e8113e9f3caa5fae5087b03ebd7af5841
kachraz <110726496+kachraz@users.noreply.github.com>
2024-12-31 01:25:29 +0000

    🫦

--- KL/exc/mac_changer.py
+++ KL/exc/mac_changer.py
@@ -3,6 +3,12 @@
 # dependencies = []
 # ///
 
+# ---
+"""
+mac_changer.py - A script to change the MAC address of a network interface for kl testing
+"""
+# ---
+
 
 def main() -> None:
     print("Hello from mac_changer.py!")
