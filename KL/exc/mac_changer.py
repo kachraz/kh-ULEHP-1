0e403947d713106f4f3cc5cf4cc6cff9f74daf55
kachraz <110726496+kachraz@users.noreply.github.com>
2024-12-31 02:02:18 +0000

    🫦

--- KL/exc/mac_changer.py
+++ KL/exc/mac_changer.py
@@ -24,8 +24,8 @@ def main() -> None:
 def subp1():
     label("mac_changer")
     """Sunprocess function test"""
-    subprocess.call(["ifconfig"], shell=True)
-    subprocess.call(["macchanger -r eth0"], shell=True)
+    subprocess.call(["ifconfig"])
+    subprocess.call(["macchanger -r eth0"])
 
 
 # Excute the main function
