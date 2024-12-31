a2923e680a42ee5256e26c83d9e65b44079101a1
kachraz <110726496+kachraz@users.noreply.github.com>
2024-12-31 02:22:52 +0000

    🫦

--- KL/exc/macchanger/src/m1.py
+++ KL/exc/macchanger/src/m1.py
@@ -5,7 +5,7 @@ from src.utils import label
 
 
 def mac1():
-    label("mac_changer")
+    label("mac_changer version 1")
     """Sunprocess function test"""
     subprocess.run(["ifconfig"])
     rprint("[cyan] Executing the macchanger three times[/cyan]")
