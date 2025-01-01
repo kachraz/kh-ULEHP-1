841f60f3eaa07eff995f1ec690dd98f2075993f2
kachraz <110726496+kachraz@users.noreply.github.com>
2025-01-01 18:09:09 +0000

    🫦

---
+++ KL/fux.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+clear
+
+# Colors
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export WHITE='\033[0;37m'
+export NC='\033[0m' # No Color
+
+# Commands
+b1() {
+    echo -e "${CYAN} ======================"
+    echo -e "${RED} Rape Commands Test${NC}"
+    echo -e " ======================${NC}"
+}
+
+# Execution Sequence
+b1
