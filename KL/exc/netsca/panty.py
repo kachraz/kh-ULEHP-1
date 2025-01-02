1cb4cf4d07abd5a80807c3f47be568b1789c2ed3
kachraz <110726496+kachraz@users.noreply.github.com>
2025-01-02 19:46:15 +0000

    🫦

--- KL/exc/netsca/panty.py
+++ KL/exc/netsca/panty.py
@@ -1,18 +1,19 @@
 # Main entry point
 
 from src.utils import label
+from src.w1 import scan1
 
 from rich.traceback import install
 
 install(show_locals=True)
 
 # Variables that will be used int the functions
-IP1 = ""
+IP1 = "10.0.0.163"
 
 
 # Function definitions
 def main():
-    label("Panty")
+    scan1(IP1)
 
 
 if __name__ == "__main__":
