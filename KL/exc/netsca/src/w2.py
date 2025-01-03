8660fa700d5b91ba4b7ea33040191cbbe554861a
kachraz <110726496+kachraz@users.noreply.github.com>
2025-01-03 17:41:46 +0000

    🫦

--- KL/exc/netsca/src/w2.py
+++ KL/exc/netsca/src/w2.py
@@ -13,4 +13,13 @@ def w2scan1(ip):
     arp_request = sca.ARP()
     arp_request.pdst = ip
     rprint(arp_request.summary())
-    sca.ls(sca.ARP())
+
+
+"""
+This funct
+"""
+
+
+def w2scan2():
+    """Listing all of scapy attributes"""
+    sca.ls(sca.ARP())  # Show all the attributes
