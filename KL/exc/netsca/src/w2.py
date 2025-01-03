626903a2153473fceab5f993c457154a3b88d70c
kachraz <110726496+kachraz@users.noreply.github.com>
2025-01-03 16:26:22 +0000

    🫦

--- KL/exc/netsca/src/w2.py
+++ KL/exc/netsca/src/w2.py
@@ -1,6 +1,13 @@
 # scapy arp ping 2nd version
+# --- Imports Section ---
+
 from rich import print as rprint
-from scapy.all import *
+import scapy.all as sca
 from rich.console import Console
 
 console = Console()
+
+
+# --- Variables Section ---
+def w2scan1(ip):
+    arp_request = sca.ARP(pdst=ip)
